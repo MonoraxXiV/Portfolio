5bb12003b5f128d139a57dddd6352ef40daf4af0
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-12-15 16:25:24 +0100

    added second dropdown for javascript

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,8 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown1").classList.toggle("show");
+    document.getElementById("myDropdown2").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it

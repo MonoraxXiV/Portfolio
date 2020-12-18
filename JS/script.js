d8ac1650c4c302d37d3ad838d53d2b9bf454975c
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-12-18 15:29:02 +0100

    adding dark mode

--- JS/script.js
+++ JS/script.js
@@ -21,3 +21,27 @@ window.onclick = function(event) {
         }
     }
 }
+const html = document.getElementsByTagName('html')[0];
+
+function toggleDarkMode() {
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark');
+    } else {
+        html.classList.add('dark');
+    }
+}
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.querySelector('html').classList.add('dark')
+} else {
+    document.querySelector('html').classList.remove('dark')
+}
+
+// Whenever the user explicitly chooses light mode
+localStorage.theme = 'light'
+
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = 'dark'
+
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem('theme')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class',// or 'media' or 'class'
   theme: {
+    colors: {
+      blue: {
+        light: '#85d7ff',
+        DEFAULT: '#1fb6ff',
+        dark: '#009eeb',
+      },
+      themed: {
+        teal: 'rgba(66, 189, 186,0.5)',
+        darkTeal: '#116466',
+
+      }
+    },
+
     extend: {},
   },
   variants: {

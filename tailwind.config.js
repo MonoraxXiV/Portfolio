d1749b7d2378551f31d2043c84c3ffb7be532f32
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2021-01-14 15:20:58 +0100

    moved custom colors to extend, also imported most of the tailwind color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,42 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
-  purge: [],
-  darkMode: 'class',// or 'media' or 'class'
-  theme: {
-    colors: {
-      blue: {
-        light: '#85d7ff',
-        DEFAULT: '#1fb6ff',
-        dark: '#009eeb',
-        custom: '#116466'
-      },
-      themed: {
-        teal: 'rgba(66, 189, 186,0.5)',
-      }
-    },
+    purge: [],
+    darkMode: 'class',// or 'media' or 'class'
+    theme: {
 
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+        extend: {
+            colors: {
+                blue: {
+                    light: '#85d7ff',
+                    DEFAULT: '#1fb6ff',
+                    dark: '#009eeb',
+                    custom: '#116466'
+                },
+                themed: {
+                    teal: 'rgba(66, 189, 186,0.5)',
+                    white: '#ffffff',
+                    black: '#000000',
+                    huneven: '#D9B08C',
+                    heven: '#D1E8E2',
+                    hlighteven: '#c24914',
+                    hlightuneven: '#fc8621'
+
+                },
+                teal: colors.teal,
+                emerald: colors.emerald,
+                lime: colors.lime,
+                rose: colors.rose,
+                fuchsia: colors.fuchsia,
+                violet: colors.violet,
+                lightBlue: colors.lightBlue,
+                cyan: colors.cyan,
+                orange: colors.orange,
+                amber: colors.amber,
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
